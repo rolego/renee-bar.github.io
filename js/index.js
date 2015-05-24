05615757a8808eb4a11ad8b56959f021d6e01ce7
Reto Kaiser <reto@cargomedia.ch>
2015-05-25 00:52:08 +0200

    Add basic mustache rendering

--- js/index.js
+++ js/index.js
@@ -1,9 +1,25 @@
-renderEvents = function(eventList) {
-  console.log(eventList);
+var templateCache = {};
+renderTemplate = function(name, vars) {
+  var template = templateCache[name];
+  if (undefined === template) {
+    template = templateCache[name] = $('#template-' + name).html();
+    Mustache.parse(template);
+  }
+  return Mustache.render(template, vars);
 };
 
-fetch('/data.json').then(function(response) {
+renderEvents = function(eventList) {
+  var htmlList = eventList.map(function(event) {
+    return renderTemplate('event', {
+      name: event.description
+    });
+  });
+  return htmlList.join('');
+};
+
+fetch('/js/data.json').then(function(response) {
   return response.json();
 }).then(function(json) {
-  renderEvents(json.events)
+  var html = renderEvents(json.events);
+  $('#agenda').html(html);
 });
